633fcf7e7dcdac8116a2261dfc8dfb1a858bb223
Eric T. Dawson <ericco92@gmail.com>
2013-06-14 10:32:20 -0400

    More UI enhancements; removed separate file handling.

--- src/main/java/org/reactome/CS/x/internal/GSMATask.java
+++ src/main/java/org/reactome/CS/x/internal/GSMATask.java
@@ -21,25 +21,34 @@ import org.cytoscape.work.util.ListSingleSelection;
 
 public class GSMATask extends AbstractTask
 {
-    @Tunable(description="FI Network Version")
+    
+    @Tunable (description="Reactome FI needs a fresh session to build the FI network.\n\n"
+    		+ " Would you like to save your current session?", groups="Session")
+    public boolean isNewSession = false;
+    
+    @Tunable (description="Save session as:", params="input=false;fileCategory=session",
+	    dependsOn="isNewSession=true", groups="Session")
+    public File sessionSaveFile;
+    
+    @Tunable(description="FI Network Version", groups="FI Network Construction")
     public ListSingleSelection<Integer> FI_NETWORK_VERSION = new ListSingleSelection<Integer> (2012, 2009);
     
-    @Tunable(description="File Format")
+    @Tunable(description="File Format", xorChildren=true, groups="File")
     public ListSingleSelection<String> FILE_FORMAT = new ListSingleSelection<String>("Gene Set", "Gene/Sample Number Pair", "NCI MAF");
     
-    @Tunable(description="File to analyze", params="fileCategory=network;input=true")
+    @Tunable(description="File to analyze", params="fileCategory=unspecified;input=true", groups="File")
     public File dataFile;
     
-    @Tunable(description="Sample Cutoff")
+    @Tunable(description="Sample Cutoff", dependsOn="File Format=NCI MAF", groups="File", params="")
     public int SAMPLE_CUTOFF = 2;
     
-    @Tunable(description="Fetch FI Annotations", groups="FI Network Construction Parameters")
+    @Tunable(description="Fetch FI Annotations (Warning: Slow)", groups="FI Network Construction")
     public boolean FETCH_FI_ANNOTATIONS = false;
     
-    @Tunable(description="Use Linker Genes", groups="FI Network Construction Parameters")
+    @Tunable(description="Use Linker Genes", groups="FI Network Construction")
     public boolean USE_LINKERS = false;
     
-    @Tunable(description="Show genes not linked to others", groups="FI Network Construction Parameters")
+    @Tunable(description="Show genes not linked to others", groups="FI Network Construction")
     public boolean SHOW_UNLINKED_GENES = false;
 
     private CyNetworkManager netManager;
@@ -67,31 +76,26 @@ public class GSMATask extends AbstractTask
     public void run(TaskMonitor taskMonitor) throws Exception
     {
 	System.out.println(System.currentTimeMillis());
-	if (getIsNewSession() == true)
-	{
-	    tm.execute(saveSessionAsTaskFactory.createTaskIterator());
-	}
-	Collection<FileChooserFilter> filters = new HashSet<FileChooserFilter>();
-	FileChooserFilter mafFilter = new FileChooserFilter("NCI MAF Files", "maf");
-	filters.add(mafFilter);
-	File dataFile = fileUtil.getFile(desktopApp.getJFrame(), "Please Choose A File", FileUtil.LOAD, filters);
-	if (this.dataFile == null)
-	{
-	    return;
-	}
+//	Collection<FileChooserFilter> filters = new HashSet<FileChooserFilter>();
+//	FileChooserFilter mafFilter = new FileChooserFilter("NCI MAF Files", "maf");
+//	filters.add(mafFilter);
+//	File dataFile = fileUtil.getFile(desktopApp.getJFrame(), "Please Choose A File", FileUtil.LOAD, filters);
+//	if (this.dataFile == null)
+//	{
+//	    return;
+//	}
+	
 	
     }
     
-    public boolean isNewSession = true;
-    @Tunable (description="Reactome FI needs a fresh session to build the FI network.\n\n"
-    		+ "Would you like to save your current session?", groups="Session")
-    public boolean getIsNewSession()
-    {
-	return isNewSession;
-    }
-    public void setIsNewSession(boolean newIsNewSession)
-    {
-	isNewSession = newIsNewSession;
-    }
+    
+//    public boolean getIsNewSession()
+//    {
+//	return isNewSession;
+//    }
+//    public void setIsNewSession(boolean newIsNewSession)
+//    {
+//	isNewSession = newIsNewSession;
+//    }
 
 }
