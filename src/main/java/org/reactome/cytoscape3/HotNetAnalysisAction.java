47fe3e3a777b919520320371ba885882c9adc900
Eric T. Dawson <eric.t.dawson@gmail.com>
2013-07-18 18:34:58 -0400

    Reorganized HotNetAnalysis

--- src/main/java/org/reactome/cytoscape3/HotNetAnalysisAction.java
+++ src/main/java/org/reactome/cytoscape3/HotNetAnalysisAction.java
@@ -40,7 +40,7 @@ public class HotNetAnalysisAction extends FICytoscapeAction
     {
 
         @Override
-        public void run(TaskMonitor arg0) throws Exception
+        public void run(TaskMonitor tm) throws Exception
         {
             // TODO Auto-generated method stub
             
