b3524775d394dac815948150cf3ccdbc6f9a8d81
wug <wug@bicb288.ohsu.edu>
2018-11-12 13:10:47 -0800

    pushed before switching to the master branch

--- lib/installJar.sh
+++ lib/installJar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Install a jar into the local maven repository. All parameters should be provided by the argument
-/Users/gwu/ProgramFiles/apache-maven-3.1.1/bin/mvn install:install-file -Dfile=$1 -DgroupId=$2 -DartifactId=$3 -Dversion=$4 -Dpackaging=jar
+/Users/wug/ProgramFiles/apache-maven-3.5.0/bin/mvn install:install-file -Dfile=$1 -DgroupId=$2 -DartifactId=$3 -Dversion=$4 -Dpackaging=jar
